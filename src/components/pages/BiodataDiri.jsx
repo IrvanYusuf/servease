5b3aa2407425895b297ac35d703cae051f5c1699
Tatsumi221130642 <221130642@students.mikroskil.ac.id>
2023-11-10 16:26:50 +0700

    push file bio

--- src/components/pages/BiodataDiri.jsx
+++ src/components/pages/BiodataDiri.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
-import BiodataCSS from "../../styles/pages/BiodataDiri.module.css"
-import { Button, Grid } from '@mui/material'
+import gambar from "../../assets/img/user-img.png"
+import "../../styles/pages/BiodataDiri.css"
+
 
 const BiodataDiri = () => {
   return (
-    <div className={BiodataCSS.container}>
-      <div className={BiodataCSS.HeaderProfile}>
-        <h6>Biodata Diri</h6>
-        <button type="button" className='btn'>Base class</button>
+    <div className="container-xl costum-container">
+      <div className='header'>
+        <h4>Biodata Diri</h4>
+        <button className='btn costum-btn'>Ubah</button>
+      </div>
+      <div className='container box-container'>
+        <div className='costum-card'>
+          <div>
+            <div className='bagianFoto'>
+              <img src={gambar} className='costum-img' alt='userImage'></img>
+              <div className='BioData'>
+                <p>Nama Depan </p>
+                <p>Nama Belakang</p>
+                <p>Tanggal Lahir</p>
+                <p>Jenis Kelamin</p>
+                <p>Email</p>
+                <p>Nomor Hp</p>
+              </div>
+            </div>
+            <button className='btn pilihFoto'>Ubah Foto</button>
+          </div>
+        </div>
       </div>
     </div>
   )
