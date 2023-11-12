566f0fb0c0ce0f664f69cc2f81c4da8b7ba200cc
@noviacahayamahara29 <noviacahayamahara29@gmail.com>
2023-11-12 14:05:11 +0700

    fix navigation on profile layout

--- src/components/organisms/Navigation.jsx
+++ src/components/organisms/Navigation.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import * as icon from "react-bootstrap-icons"
+import "../../styles/molecules/navigation.css"
 
 const Navigation = () => {
     const location=useLocation()
@@ -9,16 +11,16 @@ const Navigation = () => {
         <>
          <Nav variant="pills" defaultActiveKey={location.pathname} className="flex-column">
             <Nav.Item>
-              <Nav.Link href="/profile">Biodata Diri</Nav.Link>
+              <Nav.Link href="/profile"><icon.PersonCircle size={"20"} className="icon"/> Biodata Diri</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/profile/riwayat-pemesanan">Riwayat Pemesanan</Nav.Link>
+              <Nav.Link href="/profile/riwayat-pemesanan"><icon.ClockHistory size={"20"} className="icon"/>Riwayat Pemesanan</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/profile/daftar-alamat">Daftar Alamat</Nav.Link>
+              <Nav.Link href="/profile/daftar-alamat"><icon.GeoAlt size={"20"} className="icon"/>Daftar Alamat</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/login">Logout</Nav.Link>
+              <Nav.Link href="/login"><icon.BoxArrowDownLeft size={"20"} className="icon"/>Logout</Nav.Link>
             </Nav.Item>
           </Nav>  
         </>
