81e2b84cbde3cc499faea58c1ef1292a5a63c9ce
IrvanYusuf <221130031@students.mikroskil.ac.id>
2023-11-14 02:14:02 +0700

    push

--- src/components/pages/DaftarAlamat.jsx
+++ src/components/pages/DaftarAlamat.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ActionButton from "../atoms/ActionButton";
 import iconLocationNotFound from "../../assets/icon/location-not-found.png";
+import ModalFormTambahAlamat from "../molecules/ModalFormTambahAlamat";
 
 const DaftarAlamat = () => {
+  const [showModalAlamat, setShowModalAlamat] = useState(false);
+
+  const handleShowModalAlamat = () => {
+    setShowModalAlamat(true);
+  };
+  const handleCloseModalAlamat = () => {
+    setShowModalAlamat(false);
+  };
   return (
     <div className="w-100 h-100 d-flex flex-column">
       <div className="d-flex justify-content-between py-3">
         <h4 style={{ textAlign: "left" }}>Daftar Alamat</h4>
         <div>
-          <ActionButton text={"+ Tambah Alamat"} onClick={() => {}} />
+          <ActionButton
+            text={"+ Tambah Alamat"}
+            onClick={handleShowModalAlamat}
+          />
+          <ModalFormTambahAlamat
+            show={showModalAlamat}
+            onHide={handleCloseModalAlamat}
+          />
         </div>
       </div>
       <div className="d-flex flex-column justify-content-center align-items-center h-100">
